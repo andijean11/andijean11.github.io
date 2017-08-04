bde1af568f8fbab1a3f3598cd5bcf8fb94a54b8e
andijean11 <andijean11@gmail.com>
2017-08-04 12:06:19 -0500

    Add files via upload
    version as of 8/4/17

---
+++ scripts.js
@@ -0,0 +1,382 @@
+/*FOR GP1*/
+
+$(document).ready(function(){
+ $(".frag1").click(function(){
+   $(".frag1").toggleClass('black');
+ });
+ });
+$(document).ready(function(){
+  $(".frag2").click(function(){
+    $(".frag2").toggleClass('black');
+  });
+  })
+;
+$(document).ready(function(){
+   $(".frag3").click(function(){
+     $(".frag3").toggleClass('black');
+   });
+ });
+ $(document).ready(function(){
+  $(".frag4").click(function(){
+    $(".frag4").toggleClass('black');
+  });
+  });
+  $(document).ready(function(){
+   $(".frag5").click(function(){
+     $(".frag5").toggleClass('black');
+   });
+   });
+  $(document).ready(function(){
+    $(".frag6").click(function(){
+      $(".frag6").toggleClass('black');
+    });
+    })
+  ;
+  $(document).ready(function(){
+     $(".frag7").click(function(){
+       $(".frag7").toggleClass('black');
+     });
+   });
+   $(document).ready(function(){
+    $(".frag8").click(function(){
+      $(".frag8").toggleClass('black');
+    });
+    });
+    $(document).ready(function(){
+     $(".frag9").click(function(){
+       $(".frag9").toggleClass('black');
+     });
+     });
+    $(document).ready(function(){
+      $(".frag10").click(function(){
+        $(".frag10").toggleClass('black');
+      });
+      })
+    ;
+    $(document).ready(function(){
+       $(".frag11").click(function(){
+         $(".frag11").toggleClass('black');
+       });
+     });
+     $(document).ready(function(){
+      $(".frag12").click(function(){
+        $(".frag12").toggleClass('black');
+      });
+      });
+
+
+/*fp4*/
+      $(document).ready(function(){
+         $(".word1").hover(function(){
+           $(".word1").toggleClass('highlight');
+         });
+       });
+       $(document).ready(function(){
+          $(".word2").hover(function(){
+            $(".word2").toggleClass('highlight');
+          });
+        });
+
+        $(document).ready(function(){
+           $(".word3").hover(function(){
+             $(".word3").toggleClass('highlight');
+           });
+         });
+
+         $(document).ready(function(){
+            $(".word4").hover(function(){
+              $(".word4").toggleClass('highlight');
+              $(".word4a").toggleClass('highlight2');
+            });
+          });
+
+          $(document).ready(function(){
+             $(".word4a").hover(function(){
+               $(".word4").toggleClass('highlight');
+               $(".word4a").toggleClass('highlight2');
+             });
+           });
+
+           $(document).ready(function(){
+              $(".word5").hover(function(){
+                $(".word5").toggleClass('highlight');
+                $(".word5a").toggleClass('highlight2');
+              });
+            });
+
+            $(document).ready(function(){
+               $(".word5a").hover(function(){
+                 $(".word5").toggleClass('highlight');
+                 $(".word5a").toggleClass('highlight2');
+               });
+             });
+
+             $(document).ready(function(){
+                $(".word6").hover(function(){
+                  $(".word6").toggleClass('highlight');
+                });
+              });
+
+              $(document).ready(function(){
+                 $(".word7").hover(function(){
+                   $(".word7").toggleClass('highlight');
+                 });
+               });
+
+               $(document).ready(function(){
+                  $(".word8").hover(function(){
+                    $(".word8").toggleClass('highlight');
+                    $(".word8a").toggleClass('highlight2');
+                  });
+                });
+
+                $(document).ready(function(){
+                   $(".word8a").hover(function(){
+                     $(".word8").toggleClass('highlight');
+                     $(".word8a").toggleClass('highlight2');
+                   });
+                 });
+
+                 $(document).ready(function(){
+                    $(".word9").hover(function(){
+                      $(".word9").toggleClass('highlight');
+                      $(".word9a").toggleClass('highlight2');
+                    });
+                  });
+
+                  $(document).ready(function(){
+                     $(".word9a").hover(function(){
+                       $(".wor9").toggleClass('highlight');
+                       $(".word9a").toggleClass('highlight2');
+                     });
+                   });
+
+                   $(document).ready(function(){
+                      $(".word10").hover(function(){
+                        $(".word10").toggleClass('highlight');
+                      });
+                    });
+                    $(document).ready(function(){
+                       $(".word11").hover(function(){
+                         $(".word11").toggleClass('highlight');
+                         $(".word11a").toggleClass('highlight2');
+                       });
+                     });
+
+                     $(document).ready(function(){
+                        $(".word11a").hover(function(){
+                          $(".wor11").toggleClass('highlight');
+                          $(".word11a").toggleClass('highlight2');
+                        });
+                      });
+
+                      $(document).ready(function(){
+                         $(".word12").hover(function(){
+                           $(".word12").toggleClass('highlight');
+                           $(".word12a").toggleClass('highlight2');
+                         });
+                       });
+
+                       $(document).ready(function(){
+                          $(".word12a").hover(function(){
+                            $(".wor12").toggleClass('highlight');
+                            $(".word12a").toggleClass('highlight2');
+                          });
+                        });
+
+                        $(document).ready(function(){
+                           $(".word13").hover(function(){
+                             $(".word13").toggleClass('highlight');
+                             $(".word13a").toggleClass('highlight2');
+                           });
+                         });
+
+                         $(document).ready(function(){
+                            $(".word13a").hover(function(){
+                              $(".wor13").toggleClass('highlight');
+                              $(".word13a").toggleClass('highlight2');
+                            });
+                          });
+
+                          $(document).ready(function(){
+                             $(".word14").hover(function(){
+                               $(".word14").toggleClass('highlight');
+                             });
+                           });
+
+                           $(document).ready(function(){
+                              $(".word15").hover(function(){
+                                $(".word15").toggleClass('highlight');
+                              });
+                            });
+
+                            $(document).ready(function(){
+                               $(".word16").hover(function(){
+                                 $(".word16").toggleClass('highlight');
+                               });
+                             });
+
+                             $(document).ready(function(){
+                                $(".word17").hover(function(){
+                                  $(".word17").toggleClass('highlight');
+                                });
+                              });
+
+
+                              $(document).ready(function(){
+                                 $("#highlightAll").click(function(){
+                                   $(".word1").toggleClass('highlight3');
+                                    $(".word2").toggleClass('highlight3');
+                                     $(".word3").toggleClass('highlight3');
+                                      $(".word4").toggleClass('highlight3');
+                                       $(".word5").toggleClass('highlight3');
+                                        $(".word6").toggleClass('highlight3');
+                                         $(".word7").toggleClass('highlight3');
+                                          $(".word8").toggleClass('highlight3');
+                                           $(".word9").toggleClass('highlight3');
+                                            $(".word10").toggleClass('highlight3');
+                                             $(".word11").toggleClass('highlight3');
+                                              $(".word12").toggleClass('highlight3');
+                                               $(".word13").toggleClass('highlight3');
+                                                $(".word14").toggleClass('highlight3');
+                                                 $(".word15").toggleClass('highlight3');
+                                                  $(".word16").toggleClass('highlight3');
+                                                   $(".word17").toggleClass('highlight3');
+                                                   $(".word4a").toggleClass('highlight4');
+                                                    $(".word5a").toggleClass('highlight4');
+                                                     $(".word8a").toggleClass('highlight4');
+                                                      $(".word9a").toggleClass('highlight4');
+                                                       $(".word11a").toggleClass('highlight4');
+                                                        $(".word12a").toggleClass('highlight4');
+                                                         $(".word13a").toggleClass('highlight4');
+
+                                 });
+                               });
+
+
+/* for fp5*/
+$(document).ready(function(){
+   $(".word20").hover(function(){
+     $(".word20").toggleClass('highlight');
+   });
+ });
+ $(document).ready(function(){
+    $(".word21").hover(function(){
+      $(".word21").toggleClass('highlight');
+      $(".word21a").toggleClass('highlight2');
+    });
+  });
+
+  $(document).ready(function(){
+     $(".word21a").hover(function(){
+       $(".word21").toggleClass('highlight');
+       $(".word21a").toggleClass('highlight2');
+     });
+   });
+   $(document).ready(function(){
+      $(".word22").hover(function(){
+        $(".word22").toggleClass('highlight');
+        $(".word22a").toggleClass('highlight2');
+      });
+    });
+
+    $(document).ready(function(){
+       $(".word22a").hover(function(){
+         $(".word22").toggleClass('highlight');
+         $(".word22a").toggleClass('highlight2');
+       });
+     });
+     $(document).ready(function(){
+        $(".word23").hover(function(){
+          $(".word23").toggleClass('highlight');
+          $(".word23a").toggleClass('highlight2');
+        });
+      });
+
+      $(document).ready(function(){
+         $(".word23a").hover(function(){
+           $(".word23").toggleClass('highlight');
+           $(".word23a").toggleClass('highlight2');
+         });
+       });
+       $(document).ready(function(){
+          $(".word24").hover(function(){
+            $(".word24").toggleClass('highlight');
+          });
+        });
+
+        $(document).ready(function(){
+           $(".word25").hover(function(){
+             $(".word25").toggleClass('highlight');
+           });
+         });
+         $(document).ready(function(){
+            $(".word26").hover(function(){
+              $(".word26").toggleClass('highlight');
+            });
+          });
+          $(document).ready(function(){
+             $(".word27").hover(function(){
+               $(".word27").toggleClass('highlight');
+             });
+           });
+           $(document).ready(function(){
+              $(".word28").hover(function(){
+                $(".word28").toggleClass('highlight');
+              });
+            });
+            $(document).ready(function(){
+               $(".word29").hover(function(){
+                 $(".word29").toggleClass('highlight');
+                 $(".word29a").toggleClass('highlight2');
+               });
+             });
+
+             $(document).ready(function(){
+                $(".word29a").hover(function(){
+                  $(".word29").toggleClass('highlight');
+                  $(".word29a").toggleClass('highlight2');
+                });
+              });
+
+              $(document).ready(function(){
+                 $(".word30").hover(function(){
+                   $(".word30").toggleClass('highlight');
+                   $(".word30a").toggleClass('highlight2');
+                 });
+               });
+
+               $(document).ready(function(){
+                  $(".word30a").hover(function(){
+                    $(".word30").toggleClass('highlight');
+                    $(".word30a").toggleClass('highlight2');
+                  });
+                });
+                $(document).ready(function(){
+                   $(".word31").hover(function(){
+                     $(".word31").toggleClass('highlight');
+                   });
+                 });
+
+                 $(document).ready(function(){
+                    $("#highlightAll").click(function(){
+                      $(".word20").toggleClass('highlight3');
+                       $(".word21").toggleClass('highlight3');
+                        $(".word22").toggleClass('highlight3');
+                         $(".word23").toggleClass('highlight3');
+                          $(".word24").toggleClass('highlight3');
+                           $(".word25").toggleClass('highlight3');
+                            $(".word26").toggleClass('highlight3');
+                             $(".word27").toggleClass('highlight3');
+                              $(".word28").toggleClass('highlight3');
+                               $(".word29").toggleClass('highlight3');
+                                $(".word30").toggleClass('highlight3');
+                                 $(".word31").toggleClass('highlight3');
+                                      $(".word21a").toggleClass('highlight4');
+                                       $(".wor22a").toggleClass('highlight4');
+                                        $(".word23a").toggleClass('highlight4');
+                                        $(".word29a").toggleClass('highlight4');
+                                        $(".word30a").toggleClass('highlight4');
+
+                    });
+                  });
